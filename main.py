1229ce4822602032c3aaf53d0edb384752c5fd97
Raaid Elmelegy <Elmelegy_raaid@student.smc.edu>
2022-02-26 19:58:38 -0800

    new comit

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ client = discord.Client()
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@client.event 
+@client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startwith('$hello'):
+
+    if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
-client.run("OTQ3MzE5ODg4NDI5NTkyNjU3.YhriZQ.daSvQe3ljYVMxWdQE2fibkQmPhA")
+
+client.run(os.getenv("OTQ3MzE5ODg4NDI5NTkyNjU3.YhriZQ.daSvQe3ljYVMxWdQE2fibkQmPhA"))
